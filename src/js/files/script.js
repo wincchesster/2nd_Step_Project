2033d5812fdeb0986f05845a7c716dfd9f244c44
Alexandr Chaikovsky <office2creative@gmail.com>
2023-07-09 19:33:44 +0300

    added animation to picture on the features section

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,23 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+// слайд картинки в секції features-section__reel (переміщення картинки вліво при прокрутці)
+window.addEventListener('scroll', function() {
+    let image = document.querySelector('.slide-in-image');
+    let imagePosition = image.getBoundingClientRect().top;
+  
+    let windowHeight = window.innerHeight;
+  
+    if (imagePosition - windowHeight <= 0) {
+      image.classList.add('slide-in');
+    }
+  });
+
+//   function color() {
+//     let img = document.querySelector('.slide-in-image');
+
+//     img.classList.add('slide-in');
+//   }
+
+//   color();
